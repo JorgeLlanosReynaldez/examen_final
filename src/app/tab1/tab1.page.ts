5db88712be16ca16ab4feaeaa2d253651da1bd99
jorge <jorgelllreynaldez@gmail.com>
2020-08-26 20:40:20 -0400

    Dados del hombre de lata

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  dado1 = 0;
+  dado2 = 0;
+  dado3 = 0;
+  dado4 = 0;
+  suma = 0;
+  eleccion = '';
+  resultado: string;
+
   constructor() {}
+
+  calcularSuma() {
+
+    this.dado1 = Math.floor(Math.random() * 3) + 1;
+    this.dado2 = Math.floor(Math.random() * 3) + 1;
+    this.dado3 = Math.floor(Math.random() * 3) + 1;
+    this.dado4 = Math.floor(Math.random() * 3) + 1;
+
+    this.suma = this.dado1 + this.dado2 + this.dado3 + this.dado4;
+    if (this.suma == 12) {
+      this.resultado = 'ganaste se repite';
+    }
+    else if (this.suma !== 12) {
+      this.resultado = 'perdiste no se repite';
+    }
+    
+    // En php: $this->suma = $this->dado1 + $this->dado2
+  }
+
+  elegirOpcion(opcion: string) {
+    this.eleccion = opcion;
+  }
 
 }
